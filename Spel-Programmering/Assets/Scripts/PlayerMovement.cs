1ac22b460bdc508b5f4dab72a976131be66c80bf
HugoFredriksson <89245482+HugoFredriksson@users.noreply.github.com>
2022-03-24 18:25:40 +0100

    Fixade rörelseanimationer och låste kameran på x-axeln
    Låste kameran på x-axeln för all rörelse inom spelet kommer att ske via y-axeln; alltså uppåt så ingen rörelse av kameran på sidan är nödvändig.

--- Spel-Programmering/Assets/Scripts/PlayerMovement.cs
+++ Spel-Programmering/Assets/Scripts/PlayerMovement.cs
@@ -5,20 +5,62 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator anim;
+    private float moveSpeed = 7f;
+    private float jumpForce = 12f;
+    private SpriteRenderer sprite;
+    private float dirX = 0f;
+    private enum MovementState { idle, running, jumping, falling } /* Behövs för att få variabler som kan enkelt användas inom animatorn i unity, gör det också enklare att byta mellan olika animationer */
+    
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        float dirX = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
         if (Input.GetButtonDown("Jump"))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 12, 0);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
+
+        UpdateAnimationUpdate();
+    }
+        private void UpdateAnimationUpdate()
+        {  
+
+        MovementState state;   
+        
+        if (dirX > 0f)
+        {
+            state = MovementState.running;
+            sprite.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+                state = MovementState.running;
+                sprite.flipX = true;
+        }
+        else
+        {
+            state = MovementState.idle;
+        }
+         
+        if (rb.velocity.y > .1f)
+        {
+            state = MovementState.jumping;
+        }
+        else if (rb.velocity.y < -.1f)
+        {
+             state = MovementState.falling;
+        }
+
+        anim.SetInteger("state", (int)state);
     }
 }
