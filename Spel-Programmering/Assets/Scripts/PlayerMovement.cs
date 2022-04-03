be81c9f46c7ca3db4a67cbb781ba596bdda30529
HugoFredriksson <89245482+HugoFredriksson@users.noreply.github.com>
2022-04-03 17:06:42 +0200

    Groundcheck med Boxcast implementerat.

--- Spel-Programmering/Assets/Scripts/PlayerMovement.cs
+++ Spel-Programmering/Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private BoxCollider2D coll;
     private Animator anim;
+    [SerializeField] private LayerMask jumpableGround;
     private float moveSpeed = 7f;
     private float jumpForce = 12f;
     private SpriteRenderer sprite;
@@ -17,6 +19,7 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
+        coll = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -25,7 +28,7 @@ public class PlayerMovement : MonoBehaviour
         dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && IsGrounded()) // Ifall desa två conditions uppmöts så utförs funktionen //
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
@@ -62,5 +65,10 @@ public class PlayerMovement : MonoBehaviour
         }
 
         anim.SetInteger("state", (int)state);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround); // Skapar en "låda" runt spelaren som har samma form som boxcollidern, lådan kan checka om något overidar med lådan isåfall är den på marken //
     }
 }
