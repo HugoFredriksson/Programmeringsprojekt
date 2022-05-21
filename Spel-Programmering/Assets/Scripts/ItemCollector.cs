5d819c15ccc1ff2ca8c087617c26bd4e16acbd86
HugoFredriksson <89245482+HugoFredriksson@users.noreply.github.com>
2022-05-21 15:37:58 +0200

    Collection system 100% färdig med c# som påverkar text i unity

--- Spel-Programmering/Assets/Scripts/ItemCollector.cs
+++ Spel-Programmering/Assets/Scripts/ItemCollector.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class ItemCollector : MonoBehaviour
 {
+    [SerializeField] private Text crystalText;
+
    public int crystals = 0;
     private void OnTriggerEnter2D(Collider2D collision) /* Använder IsTrigger på kristallen när man plocker upp den och då är det mest lämpat att använda Collider2D */
     {
@@ -11,7 +13,7 @@ public class ItemCollector : MonoBehaviour
         {
            Destroy(collision.gameObject);
            crystals++;
-           Debug.Log("Crystals:" + crystals); 
+           crystalText.text = "Crystals:" + crystals;
 
         }
     }
